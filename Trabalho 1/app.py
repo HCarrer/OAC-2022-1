b5a9427cbbe22167275d83440fbec21c16fe8463
HCarrer <hmcarrer@gmail.com>
2022-07-16 12:45:06 -0300

    implements general instruction mounting method

--- Trabalho 1/app.py
+++ Trabalho 1/app.py
@@ -62,8 +62,24 @@ I_STRUCTURE = ['op', 'rs', 'rt', 'immediate']
 
 J_STRUCTURE = ['op', 'address']
 
+STRUCTURES = {
+  'R': ['op', 'rs', 'rt', 'rd', 'shamt', 'funct'],
+  'I': ['op', 'rs', 'rt', 'immediate'],
+  'J': ['op', 'address']
+}
+
 OPCODES = {
   'R': '000000',
+  'I': {
+    'addi': '001000',
+    'addiu': '001001',
+    'andi': '001100',
+    'beq': '000100',
+    'bgez': '000001',
+    'bgtz': '000111',
+    'blez': '000110',
+    'bltz': '000001',
+  }
 }
 
 R_FUNCTION_CODES = {
@@ -113,16 +129,25 @@ def get_type(instruction):
     return 'I'
   return 'J'
 
-def get_type_structure(type):
-  if type == 'R':
-    return R_STRUCTURE
-  if type == 'I':
-    return I_STRUCTURE
-  return J_STRUCTURE
+# def get_type_structure(type):
+#   if type == 'R':
+#     return R_STRUCTURE
+#   if type == 'I':
+#     return I_STRUCTURE
+#   return J_STRUCTURE
+
+def make_it_n_bits(binary, size):
+  if len(binary) < size:
+    binary = '0' + binary
+    new_binary = make_it_n_bits(binary, size)
+  else:
+    new_binary = binary
+  return new_binary
 
 def get_register(register):
   decimal_index = REGISTERS.index(register)
-  return bin(decimal_index)[2:]
+  binary = bin(decimal_index)[2:]
+  return make_it_n_bits(binary, 5)
 
 def get_opcode(instruction):
   type = get_type(instruction)
@@ -167,22 +192,42 @@ def get_funct(instruction):
   if type =='R':
     return R_FUNCTION_CODES[command]
 
-def mount_R_type(instruction):
-  # R_STRUCTURE['op'] = get_opcode(instruction)
-  # R_STRUCTURE['rs'] = get_rs(instruction)
-  # R_STRUCTURE['rs'] = get_rs(instruction)
-  # R_STRUCTURE['rs'] = get_rs(instruction)
-  op = get_opcode(instruction)
-  rs = get_rs(instruction)
-  rt = get_rt(instruction)
-  rd = get_rd(instruction)
-  shamt = get_shamt(instruction)
-  funct = get_funct(instruction)
-  print(op, rs, rt, rd, shamt, funct)
+def get_immediate(instruction):
+  binary = bin(instruction)[2:]
+  return make_it_n_bits(binary, 16)
 
-instruction = 'add $s0, $s1, $s2'
-type = get_type(instruction)
-type_structure = get_type_structure(type)
+# def mount_R_type(instruction):
+#   op = get_opcode(instruction)
+#   rs = get_rs(instruction)
+#   rt = get_rt(instruction)
+#   rd = get_rd(instruction)
+#   shamt = get_shamt(instruction)
+#   funct = get_funct(instruction)
+#   print(op, rs, rt, rd, shamt, funct)
+
+def mount_instruction(instruction):
+  type = get_type(instruction)
+  structure = STRUCTURES.get(type)
+  binary_set = []
+  for field in structure:
+    if field == 'op':
+      x = get_opcode(instruction)
+    if field == 'rs':
+      x = get_rs(instruction)
+    if field == 'rt':
+      x = get_rt(instruction)
+    if field == 'rd':
+      x = get_rd(instruction)
+    if field == 'shamt':
+      x = get_shamt(instruction)
+    if field == 'funct':
+      x = get_funct(instruction)
+    if field == 'immediate':
+      x = get_immediate(instruction)
+    binary_set.append(x)
+  print(' '.join(binary_set))
+
+instruction = 'add $t0, $s2, $t0'
 
 print(instruction)
-mount_R_type(instruction)
+mount_instruction(instruction)
